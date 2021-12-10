424caf611666b2fcac3e6ecf4f7880bcf6a2af59
Nixon2223 <n.nixon@hotmail.co.uk>
2021-12-10 14:10:19 +0000

    db setup

---
+++ gym_manager/db/gym_manager.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS class;
+DROP TABLE IF EXISTS coach;
+DROP TABLE IF EXISTS member;
+
+CREATE TABLE member (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE coach (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE class (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    sport VARCHAR(255),
+    capacity INT,
+    date DATE,
+    start VARCHAR(255),
+    end VARCHAR(255)
+);
+
+CREATE TABLE booking (
+    id SERIAL PRIMARY KEY,
+    booking_class INT REFERENCES class_(id),
+    booking_member INT REFERENCES member_(id)
+);
