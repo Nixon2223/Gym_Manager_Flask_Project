9ed7853b271ac2dc1f8d40255975faeb49be15eb
Nixon2223 <n.nixon@hotmail.co.uk>
2021-12-12 15:40:48 +0000

    members restful routes and index.html added

--- gym_manager/app.py
+++ gym_manager/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 # from controllers.booking_controller import booking_blueprint
-# from controllers.member_controller import member_blueprint
+from controllers.members_controller import members_blueprint
 # from controllers.class_controller import class_blueprint
 # from controllers.coach_controller import coach_blueprint
 
 app = Flask(__name__)
 
+app.register_blueprint(members_blueprint)
 
 @app.route("/")
 def main():
---
+++ gym_manager/controllers/members_controller.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+
+from models.member import Member
+import repositories.member_repository as member_repository
+
+members_blueprint = Blueprint("members", __name__)
+
+# INDEX
+@members_blueprint.route("/members")
+def members():
+    members = member_repository.select_all()
+    return render_template("members/index.html", members=members)
+
+
+# NEW
+@members_blueprint.route("/members/new")
+def new_member():
+    return render_template("members/new.html")
+
+
+# CREATE
+@members_blueprint.route("/members", methods=["POST"])
+def create_member():
+    name = request.form["name"]
+    membership = request.form["membership"]
+    new_member = Member(name, membership)
+    member_repository.save(new_member)
+    return redirect("/members")
+
+
+# EDIT
+@members_blueprint.route("/members/<id>/edit")
+def edit_member(id):
+    member = member_repository.select(id)
+    return render_template('members/edit.html', member=member)
+
+
+# UPDATE
+@members_blueprint.route("/members/<id>", methods=["POST"])
+def update_member(id):
+    name = request.form["name"]
+    membership = request.form["membership"]
+    member = Member(name, membership, id)
+    member_repository.update(member)
+    return redirect("/members")
+
+
+# DELETE
+@members_blueprint.route("/members/<id>/delete", methods=["POST"])
+def delete_member(id):
+    member_repository.delete(id)
+    return redirect("/members")
