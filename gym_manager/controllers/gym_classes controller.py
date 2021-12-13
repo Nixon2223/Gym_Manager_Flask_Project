f418f99d6fc1667033c1bd875cbe86df72022adc
Nixon2223 <n.nixon@hotmail.co.uk>
2021-12-13 10:11:51 +0000

    gym_class repo set up

---
+++ gym_manager/controllers/gym_classes controller.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+
+from models.gymclass import GymClass
+import repositories.gym_class_repository as gym_class_repository
+import repositories.coach_repository as coach_repository
+
+gym_classes_blueprint = Blueprint("gym_classes", __name__)
+
+# INDEX
+@gym_classes_blueprint.route("/gym_classes")
+def gym_classes():
+    gym_classes = gym_class_repository.select_all()
+    return render_template("gym_classes/index.html", gym_classes=gym_classes)
+
+
+# SHOW
+@gym_classes_blueprint.route("/gym_classes/<id>")
+def show_gym_class(id):
+    victims = gym_class_repository.select_victims_of_gym_class(id)
+    gym_class = gym_class_repository.select(id)
+    return render_template("gym_classes/show.html", victims=victims, gym_class=gym_class)
+
+
+# NEW
+@gym_classes_blueprint.route("/gym_classes/new")
+def new_gym_class():
+    coaches = coach_repository.select_all()
+    return render_template("gym_classes/new.html", coaches=coaches)
+
+
+# CREATE
+@gym_classes_blueprint.route("/gym_classes", methods=["POST"])
+def create_gym_class():
+    name = request.form["name"]
+    coach_id = request.form["coach_id"]
+    coach = coach_repository.select(coach_id)
+    new_gym_class = GymClass(name, coach)
+    gym_class_repository.save(new_gym_class)
+    return redirect("/gym_classes")
+
+
+# EDIT
+@gym_classes_blueprint.route("/gym_classes/<id>/edit")
+def edit_gym_class(id):
+    gym_class = gym_class_repository.select(id)
+    coach = coach_repository.select_all()
+    return render_template('gym_classes/edit.html', gym_class=gym_class, coach=coach)
+
+
+# UPDATE
+@gym_classes_blueprint.route("/gym_classes/<id>", methods=["POST"])
+def update_gym_class(id):
+    name = request.form["name"]
+    coach_id = request.form["coach_id"]
+    coach = coach_repository.select(coach_id)
+    gym_class = GymClass(name, coach, id)
+    gym_class_repository.update(gym_class)
+    return redirect("/gym_classes")
+
+
+# DELETE
+@gym_classes_blueprint.route("/gym_classes/<id>/delete", methods=["POST"])
+def delete_gym_class(id):
+    gym_class_repository.delete(id)
+    return redirect("/gym_classes")
