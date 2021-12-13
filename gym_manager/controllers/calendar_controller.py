c607b360c3e21465eb7fcc99a9e92ce6eccea579
Nixon2223 <n.nixon@hotmail.co.uk>
2021-12-13 22:43:21 +0000

    added view class to calendar

--- gym_manager/controllers/calendar_controller.py
+++ gym_manager/controllers/calendar_controller.py
@@ -13,13 +13,20 @@ def calendar():
 #VIEW DAY
 @calendar_blueprint.route("/calendar/<day>")
 def calendar_get_day(day):
-    booked_in = []
-    for member in gym_class_repository.select_members_of_gym_class(3):
-        booked_in.append(member.id)
-    all_members = member_repository.select_all()
     all_gym_classes = gym_class_repository.select_all()
     gym_classes = []
     for gym_class in all_gym_classes:
         if str(gym_class.date)[8:] == day:
             gym_classes.append(gym_class)
-    return render_template("calendar/day.html", day = day, gym_classes = gym_classes, members = all_members, booked_in = booked_in)
+    all_members = member_repository.select_all()
+    return render_template("calendar/day.html", day = day, gym_classes = gym_classes, members = all_members)
+
+#VIEW CLASS
+@calendar_blueprint.route("/calendar/class/<id>")
+def calendar_get_class(id):
+    all_members = member_repository.select_all()
+    gym_class = gym_class_repository.select(id)
+    booked_in = []
+    for member in gym_class_repository.select_members_of_gym_class(id):
+        booked_in.append(member.id)
+    return render_template("calendar/class.html", gym_class = gym_class, booked_in = booked_in, members = all_members)
