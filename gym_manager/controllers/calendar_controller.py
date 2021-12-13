b7fc75874094aee2e2bbbc74cd00012e5860a730
Nixon2223 <n.nixon@hotmail.co.uk>
2021-12-13 14:07:39 +0000

    calendar select day working

--- gym_manager/controllers/calendar_controller.py
+++ gym_manager/controllers/calendar_controller.py
@@ -9,11 +9,12 @@ calendar_blueprint = Blueprint("calendar", __name__)
 def calendar():
     return render_template("calendar/index.html")
 
+#VIEW DAY
 @calendar_blueprint.route("/calendar/<day>")
 def calendar_get_day(day):
     all_gym_classes = gym_class_repository.select_all()
     gym_classes = []
     for gym_class in all_gym_classes:
-        if str(gym_class.date)[5:-3] == day:
+        if str(gym_class.date)[8:] == day:
             gym_classes.append(gym_class)
     return render_template("calendar/day.html", day = day, gym_classes = gym_classes)
