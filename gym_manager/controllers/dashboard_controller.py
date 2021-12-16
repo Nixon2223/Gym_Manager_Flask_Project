61f0df043aace812f10e46d845b0cf101d205d7e
Nixon2223 <n.nixon@hotmail.co.uk>
2021-12-16 00:15:08 +0000

    finish?

--- gym_manager/controllers/dashboard_controller.py
+++ gym_manager/controllers/dashboard_controller.py
@@ -8,7 +8,12 @@ dashboard_blueprint = Blueprint("dashboard", __name__)
 # INDEX
 @dashboard_blueprint.route("/")
 def dashboard():
+    all_gym_classes = gym_class_repository.select_all()
+    gym_classes = []
+    for gym_class in all_gym_classes:
+        if str(gym_class.date)[8:] == "17":
+            gym_classes.append(gym_class)
     standard_members = len(member_repository.select_standard())
     deactivated_members = len(member_repository.select_deactivated())
     premium_members = len(member_repository.select_premium())
-    return render_template("dashboard/index.html", premium_members = premium_members, standard_members = standard_members, deactivated_members = deactivated_members)
+    return render_template("dashboard/index.html", premium_members = premium_members, standard_members = standard_members, deactivated_members = deactivated_members,gym_classes = gym_classes)
